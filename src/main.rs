7a4331d5c96a134180e800d9a56e04a1efcb71bd
OOI JIA YI <ooijiayi93@gmail.com>
2024-01-02 07:11:17 +0800

    Add TCP stream to handle connection for responding harcoded "PONG"

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,12 +12,19 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+               
+                handle_connection(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(stream: &mut TcpStream) {
+    let response = "+PONG\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
